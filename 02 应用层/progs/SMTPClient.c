b839a388f37afc887e20fd6338ba4d4b6c51d235
yangxiaohei <yangxiaohei321123@163.com>
2018-09-11 01:33:48 +0800

    计算机网络应用层编程题

--- 02 应用层/progs/SMTPClient.c
+++ 02 应用层/progs/SMTPClient.c
@@ -44,6 +44,9 @@ ssize_t YHLog_err(int line, const char *fun, const char *format, ...)
 #define __DEST_MAIL_ADDR__BASE64__      "???????"   /* 这里填源邮箱 base64 后的字符串 */
 #define __AUTH_VERIFY_CODE__BASE64__    "???????"   /* 这里填源邮箱打开 SMTP 服务的授权码 base64 后的字符串 */
 
+static unsigned short port;
+static char ip[128];
+
 static char *cmd[] = {
 
     /* HELO xxx */
@@ -76,39 +79,33 @@ static char *cmd[] = {
     "\r\n.\r\n"
 };
 
-int main(int argc, char const *argv[])
+int DNS_lookup(const char *domain, char *ipbuf, size_t buf_len)
 {
-
-    if (argc != 3)
-    {
-        LOG("usage : %s <#domain> <#port>\n", argv[0]);
-        exit(1);
-    }
-
     char script[1024];
-    int len = snprintf(script, sizeof(script), "nslookup %s | grep Address | sed '1d' | head -n2 | sed 's/Address: //'", argv[1]);
+    int len = snprintf(script, sizeof(script), "nslookup %s | grep Address | sed '1d' | head -n1 | sed 's/Address: //'", domain);
     script[len] = 0;
     FILE *fp;
     if ((fp = popen(script, "r")) == NULL)
     {
         LOG("popen error");
-        exit(1);
+        return -1;
     }
-    char ip[128];
-    if (fgets(ip, sizeof(ip), fp) == NULL)
+    if (fgets(ipbuf, buf_len, fp) == NULL)
     {
         if (ferror(fp))
         {
             LOG("fgets error");
-            exit(1);
+            return -2;
         }
     }
-    len = strlen(ip);
-    if (ip[len - 1] == '\n')
-        ip[len - 1] = 0;
-    LOG("DNS lookup result : %s", ip);
+    len = strlen(ipbuf);
+    if (ipbuf[len - 1] == '\n')
+        ipbuf[len - 1] = 0;
+    return 0;
+}
 
-    unsigned short port = atoi(argv[2]);
+int TCP_connect(const char *ip, unsigned short port)
+{
     struct sockaddr_in svraddr;
     bzero(&svraddr, sizeof(svraddr));
     svraddr.sin_family = AF_INET;
@@ -116,53 +113,142 @@ int main(int argc, char const *argv[])
     if (inet_pton(AF_INET, ip, &svraddr.sin_addr) < 0)
     {
         ERRLOG("inet_pton error");
-        exit(1);
+        return -1;
     }
-
     int fd;
     if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         ERRLOG("socket error");
-        exit(1);
+        return -2;
     }    
     if (connect(fd, (struct sockaddr *)&svraddr, sizeof(svraddr)) < 0)
     {
         ERRLOG("connect error");
-        exit(1);
+        return -3;
     }
-    LOG("connect [%s:%d] succ!", ip, port);
+    return fd;
+}
 
-    ssize_t nread = 0, ntowrite = 0, nwrite = 0;
+int recv_rsp(int fd, char *rspbuf, size_t rspbuf_len)
+{
+    int nread;
+    if ((nread = read(fd, rspbuf, rspbuf_len)) < 0)
+    {
+        ERRLOG("read error");
+        return -1;
+    }
+    else if (nread == 0)
+    {
+        LOG("SMTP server [%s:%d] close connection!", ip, port);
+        close(fd);
+    }
+    rspbuf[nread] = 0;
+    return nread;
+}
+
+int read_data(int fd, char *databuf, size_t databuf_len, char *lastcmd, size_t lastcmd_len)
+{
+    int len = 0;
+    while (fgets(databuf + len, databuf_len - len, stdin) != NULL)
+    {
+        if (strstr(databuf, "\n.\n") != NULL)
+            break;
+        printf(">");
+        len = strlen(databuf);
+    }
+    bzero(lastcmd, lastcmd_len);
+    if (ferror(stdin))
+    {
+        LOG("fgets error");
+        return -1;
+    }
+    return len;
+}
+
+int read_cmd(int fd, char *cmdbuf, size_t cmdbuf_len, char *lastcmd, size_t lastcmd_len)
+{
+    if (fgets(cmdbuf, cmdbuf_len, stdin) == NULL && ferror(stdin))
+    {
+        LOG("fgets error");
+        return -1;
+    }
+    int len = snprintf(lastcmd, lastcmd_len, "%s", cmdbuf);
+    lastcmd[len] = 0;
+    return strlen(cmdbuf);
+}
+
+int send_SMTP_msg(int fd, char *msg, size_t msglen)
+{
+    int nwrite;
+    if ((nwrite = write(fd, msg, msglen)) != msglen)
+    {
+        ERRLOG("write error");
+        return -1;
+    }
+    return nwrite;
+}
+
+int mock_SMTP_msg(int fd, int i)
+{
+    int len, nwrite;
+    char tmpbuf[1024];
+    const char *begin = "******************** cmd *****************\n";
+    const char *end   = "******************************************\n";
+
+    len = snprintf(tmpbuf, sizeof(tmpbuf), "%s%s%s", begin, cmd[i], end);
+    tmpbuf[len] = 0;
+
+    write(STDOUT_FILENO, tmpbuf, len);
+
+    if ((nwrite = write(fd, cmd[i], strlen(cmd[i]))) != strlen(cmd[i]))
+    {
+        ERRLOG("write error");
+        return -1;
+    }
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
     char recvbuf[1024];
-    setbuf(stdout, NULL);
-
-    ssize_t stored_len = 0;
     char lastcmd[256];
+    int nread;
+    setbuf(stdout, NULL);
 
 #if MOCK
     int i = 0;
     int size = sizeof(cmd) / sizeof(cmd[0]);
 #endif
 
+    if (argc != 3)
+    {
+        LOG("usage : %s <#domain> <#port>\n", argv[0]);
+        exit(1);
+    }
+
+    if (DNS_lookup(argv[1], ip, sizeof(ip)) < 0)
+    {
+        LOG("DNS_lookup error!");
+        exit(1);
+    }
+    LOG("DNS lookup result : %s", ip);
+
+    port = atoi(argv[2]);
+    int fd;
+    if ((fd = TCP_connect(ip, port)) < 0)
+    {
+        LOG("TCP_connect error!");
+        exit(1);
+    }
+    LOG("connect [%s:%d] succ!", ip, port);
+
     while (1)
     {
-        /* --------------------------- recv ---------------------------- */
-
-        if ((nread = read(fd, recvbuf, sizeof(recvbuf))) < 0)
-        {
-            ERRLOG("read error");
-            exit(1);
-        }
-        else if (nread == 0)
-        {
-            LOG("SMTP server [%s:%d] close connection!", ip, port);
-            close(fd);
+        if ((nread = recv_rsp(fd, recvbuf, sizeof(recvbuf))) <= 0)
             break;
-        }
-        recvbuf[nread] = 0;
+
         LOG("recv %d bytes response from [%s:%d]", nread, ip, port);
 
-        /* echo received data */
         if (fputs(recvbuf, stdout) == EOF)
         {
             LOG("fputs error!");
@@ -172,61 +258,37 @@ int main(int argc, char const *argv[])
 #if MOCK
         if (i == size)
             break;
-#endif 
 
-        /* --------------------------- send ---------------------------- */
-
-#if MOCK
+        mock_SMTP_msg(fd, i++);
 #else
         printf(">");
+
         if (strncmp(lastcmd, "data", 4) == 0)
         {
-            int len = 0;
-            while (fgets(recvbuf + len, sizeof(recvbuf) - len, stdin) != NULL)
+            if (read_data(fd, recvbuf, sizeof(recvbuf), lastcmd, sizeof(lastcmd)) < 0)
             {
-                if (strstr(recvbuf, "\n.\n") != NULL)
-                    break;
-                printf(">");
-                len = strlen(recvbuf);
+                LOG("read_data error!");
+                goto err;
             }
-            recvbuf[len] = 0;
-            bzero(lastcmd, sizeof(lastcmd));
         }
         else
         {
-            if (fgets(recvbuf, sizeof(recvbuf), stdin) == NULL && ferror(stdin))
+            if (read_cmd(fd, recvbuf, sizeof(recvbuf), lastcmd, sizeof(lastcmd)) < 0)
             {
-                LOG("fgets error");
-                exit(1);
+                LOG("read_data error!");
+                goto err;
+                
             }
-            int len = snprintf(lastcmd, sizeof(lastcmd), "%s", recvbuf);
-            if (lastcmd[len - 1] == '\n')
-                lastcmd[len - 1] = 0;
         }
-
-        ntowrite = strlen(recvbuf);
-        if ((nwrite = write(fd, recvbuf, ntowrite)) != ntowrite)
+        if (send_SMTP_msg(fd, recvbuf, strlen(recvbuf)) < 0)
         {
-            ERRLOG("write error");
-            exit(1);
+            LOG("send_SMTP_msg error!");
+            goto err;
         }
-#endif
-
-#if MOCK
-        int len = 0;
-        const char *begin = "******************** cmd *****************\n";
-        const char *end   = "******************************************\n";
-        char tmpbuf[1024];
-        len = snprintf(tmpbuf, sizeof(tmpbuf), "%s%s%s", begin, cmd[i], end);
-        tmpbuf[len] = 0;
-        write(STDOUT_FILENO, tmpbuf, len);
-        ntowrite = strlen(cmd[i]);
-        if ((nwrite = write(fd, cmd[i], ntowrite)) != ntowrite)
-        {
-            ERRLOG("write error");
-            exit(1);
-        }
-        i++;
+        continue;
+err:
+        close(fd);
+        break;
 #endif
     }
 
