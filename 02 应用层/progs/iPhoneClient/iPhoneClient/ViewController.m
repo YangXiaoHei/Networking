bd697eb8f3f70f2772ce339cf598e71b3581096a
yangxiaohei <yangxiaohei321123@163.com>
2018-09-12 14:15:14 +0800

    计算机网络应用层-编程题

--- 02 应用层/progs/iPhoneClient/iPhoneClient/ViewController.m
+++ 02 应用层/progs/iPhoneClient/iPhoneClient/ViewController.m
@@ -20,6 +20,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *portTextField;
 @property (nonatomic,assign) pthread_t readDataThread;
 @property (nonatomic,assign) int  sockfd;
+@property (nonatomic,assign) BOOL TCPConnected;
 @end
 
 @implementation ViewController
@@ -28,6 +29,18 @@
     
     self.button.enabled = NO;
     
+    /* 关闭 TCP 连接 */
+    if (self.TCPConnected) {
+        close(self.sockfd);
+        self.stateLabel.text = @"Nothing to show";
+        [self.button setTitle:@"建立 TCP 连接" forState:UIControlStateNormal];
+        self.button.enabled = YES;
+        self.TCPConnected = NO;
+        return;
+    }
+    
+    /* 打开 TCP 连接 */
+    
     void (^ErrorBlock)(void) = ^{
         [self.view endEditing:YES];
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
@@ -35,7 +48,9 @@
             self.button.enabled = YES;
         });
     };
+    [self.button setTitle:@"关闭 TCP 连接" forState:UIControlStateNormal];
     
+    /* HTTP 请求报文 */
     NSString *httpMsg = [NSString stringWithFormat:@"GET index.html HTTP/1.1\n"];
     const char  *httpMsgCstr = [httpMsg cStringUsingEncoding:kCFStringEncodingUTF8];
     
@@ -49,17 +64,22 @@
         ErrorBlock();
         return;
     }
-    
     const char *ipAddr = [self.ipTextField.text cStringUsingEncoding:kCFStringEncodingUTF8];
     unsigned short port = (unsigned short)[self.portTextField.text intValue];
     
     self.stateLabel.text = @"开始建立 TCP 连接...";
     
+    /* 创建 TCP 套接字文件描述符 */
     if ((self.sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         self.stateLabel.text = [NSString stringWithFormat:@"Create socket error! : %s", strerror(errno)];
         ErrorBlock();
         return;
     }
+    
+    /* 设置非阻塞套接字 */
+    int flags = fcntl(self.sockfd, F_GETFL);
+    flags |= O_NONBLOCK;
+    fcntl(self.sockfd, F_SETFL);
     
     struct sockaddr_in svraddr;
     svraddr.sin_family = AF_INET;
@@ -70,15 +90,17 @@
         return;
     }
     
+    /* 发起 TCP 连接 */
     if (connect(self.sockfd, (struct sockaddr *)&svraddr, sizeof(svraddr)) < 0) {
         self.stateLabel.text = [NSString stringWithFormat:@"Connect error! : %s", strerror(errno)];
         ErrorBlock();
         return;
     }
     
+    /* 连接成功，撤下键盘，并发送 HTTP 请求报文 */
+    self.TCPConnected = YES;
     self.stateLabel.text = [NSString stringWithFormat:@"[%s:%d] TCP 连接建立成功!", ipAddr, port];
     [self.view endEditing:YES];
-    
     ssize_t nWritten;
     size_t msgLen = strlen(httpMsgCstr);
     if ((nWritten = write(self.sockfd, httpMsgCstr, msgLen)) != msgLen) {
@@ -87,37 +109,46 @@
         return;
     }
     
+    /* 创建后台非阻塞轮询读线程 */
     int err;
     if ((err = pthread_create(&_readDataThread, NULL, backgroundBusyLoop, (__bridge void *)(self))) < 0) {
         errno = err;
-        self.stateLabel.text = [NSString stringWithFormat:@"Connect error! : %s", strerror(errno)];
+        self.stateLabel.text = [NSString stringWithFormat:@"Create thread error! : %s", strerror(errno)];
         ErrorBlock();
         return;
     }
 }
+
+/* 主线程更新 UI */
+#define UpdateUI(...) \
+dispatch_async(dispatch_get_main_queue(), ^{ __VA_ARGS__ });
 
 void * backgroundBusyLoop(void *arg) {
     ViewController *vc = (__bridge ViewController *)arg;
     while (1) {
         ssize_t nRead;
         char readBuffer[1024];
-        if ((nRead = read(vc.sockfd, readBuffer, 1024)) < 0) {
+        if ((nRead = read(vc.sockfd, readBuffer, sizeof(readBuffer))) < 0) {
             if (errno != EAGAIN) {
-                vc.stateLabel.text = [NSString stringWithFormat:@"Read data fail! :%s", strerror(errno)];
+                UpdateUI( vc.stateLabel.text = [NSString stringWithFormat:@"Read data fail! :%s", strerror(errno)]; )
                 return NULL;
             } else {
-                vc.stateLabel.text = [NSString stringWithFormat:@"服务器没有回传数据"];
+                /* 服务器还没有回传数据 */
+                UpdateUI( vc.stateLabel.text = [NSString stringWithFormat:@"服务器没有回传数据"]; )
             }
-        } else if (nRead == 0) {
-            vc.stateLabel.text = [NSString stringWithFormat:@"%@ 关闭了套接字, read EOF!", vc.ipTextField.text];
-            vc.stateLabel.text = @"Nothing to show";
-            vc.button.enabled = YES;
+        } else if (nRead == 0) {  /* 服务器切断 TCP 连接 */
+            UpdateUI( vc.stateLabel.text = [NSString stringWithFormat:@"%@ 关闭了套接字, read EOF!", vc.ipTextField.text];
+                vc.stateLabel.text = @"Nothing to show";
+                vc.button.enabled = YES; )
             close(vc.sockfd);
             return NULL;
         }
+        /* 成功读到响应报文  */
         readBuffer[nRead] = 0;
-        vc.stateLabel.text = [NSString stringWithFormat:@"%zd 字节，%s",nRead, readBuffer];
-        [vc.view setNeedsDisplay];
+        NSString *readContent = [NSString stringWithFormat:@"%s", readBuffer];
+        UpdateUI( vc.stateLabel.text = [NSString stringWithFormat:@"%zd 字节，%@",nRead, readContent];
+                  vc.button.enabled = YES;
+                  [vc.view setNeedsDisplay]; )
         return NULL;
     }
 }
@@ -128,11 +159,17 @@ void * backgroundBusyLoop(void *arg) {
     self.stateLabel.text = @"Nothing to show";
     [self.button setTitle:@"建立 TCP 连接" forState:UIControlStateNormal];
     
+    /* 默认 ip/port */
     self.ipTextField.text = @"10.10.5.165";
     self.portTextField.text = @"5000";
+    
+    /* 默认是无 TCP 连接的 */
+    self.TCPConnected = NO;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    /* 撤下键盘 */
     [self.ipTextField resignFirstResponder];
     [self.portTextField resignFirstResponder];
 }
