6acdb3a268dacc85a517acaa6feb77aadbe4e698
yangxiaohei <yangxiaohei321123@163.com>
2019-01-08 23:21:33 +0800

    处理僵尸进程

--- UNP/05 TCP 客户 服务器程序示例/progs/ReadWriteTool.c
+++ UNP/05 TCP 客户 服务器程序示例/progs/ReadWriteTool.c
@@ -26,10 +26,13 @@ ssize_t __internal_read(int fd, char *c)
 {
     if (read_cnt <= 0) {
     again:
-        if ((read_cnt = read(fd, read_buf, sizeof(read_buf))) == 0)
+        if ((read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
+            if (errno == EINTR)
+                goto again;
+            else
+                return -1;
+        } else if (read_cnt == 0)
             return 0;
-        else if (read_cnt < 0 && errno == EINTR)
-            goto again; 
         read_ptr = read_buf;
     }
     read_cnt--;
