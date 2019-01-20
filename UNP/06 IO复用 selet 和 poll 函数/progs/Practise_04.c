b00df5c524d94fae6a04506ea13bb22eb299cd55
yangxiaohei <yangxiaohei321123@163.com>
2019-01-20 23:10:37 +0800

    5 ~ 8

---
+++ UNP/06 IO复用 selet 和 poll 函数/progs/Practise_04.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include "AddrTool.h"
+#include <sys/resource.h>
+
+void printClifds(int *clifds) 
+{
+    for (int i = 0; i < 5; i++) {
+        printf("%-3d", clifds[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2) {
+        printf("usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    int listenfd = -1;
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket error!");
+        exit(1);
+    }
+
+    struct sockaddr_in svraddr, cliaddr;
+    bzero(&svraddr, sizeof(svraddr));
+    svraddr.sin_family = AF_INET;
+    svraddr.sin_port = htons(atoi(argv[1]));
+    svraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(listenfd, (struct sockaddr *)&svraddr, sizeof(svraddr)) < 0) {
+        perror("bind error!");
+        exit(1);
+    }
+
+    if (listen(listenfd, 100) < 0) {
+        perror("listen error!");
+        exit(1);
+    }
+
+    fd_set rset, allset;
+    FD_ZERO(&allset);
+    FD_SET(listenfd, &allset);
+
+    int connfd = -1;
+    int maxfd = listenfd;
+    int nready = 0;
+    int maxi = 0;
+    socklen_t clilen = sizeof(cliaddr);
+
+    char buf[1024];
+    ssize_t nread = 0, nwrite = 0;
+
+    struct rlimit rl;
+    if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
+        perror("getrlimit error!");
+        exit(1);
+    }
+    printf("FD_SETSIZE = %llu; rlimit_cur = %llu, rlimit_max = %llu\n", (unsigned long long)FD_SETSIZE, rl.rlim_cur, rl.rlim_max);
+
+    rl.rlim_max = rl.rlim_cur;
+    if (setrlimit(RLIMIT_NOFILE, &rl) < 0) {
+        perror("setrlimit error!");
+        exit(1);
+    }
+
+    int clifds[FD_SETSIZE];
+    for (int i = 0; i < FD_SETSIZE; i++)
+        clifds[i] = -1;
+
+    while (1) {
+        rset = allset;
+        select_again:
+        if ((nready = select(maxfd + 1, &rset, NULL, NULL, NULL)) < 0) {
+            if (errno == EINTR)
+                goto select_again;
+            perror("select error!");
+            exit(1);
+        }
+        printf("%d fds readable ", nready);
+
+        // 新连接进来
+        if (FD_ISSET(listenfd, &rset)) {
+            printf("listenfd\n");
+            if ((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen)) < 0) {
+                perror("accept error!");
+                exit(1);
+            }
+
+            // 存储一份 connfd 以方便之后通信
+            int i = 0;
+            for (; i < FD_SETSIZE; i++) {
+                if (clifds[i] < 0) {
+                    clifds[i] = connfd;
+                    break;
+                }
+            }
+            if (i == FD_SETSIZE) {
+                printf("too many clients\n");
+                exit(1);
+            }
+
+            char *peerInfo = getPeerInfo(connfd);
+            char *addrInfo = getSockInfo(connfd);
+            printf("%s <---> %s\n", addrInfo, peerInfo);
+            free(peerInfo);
+            free(addrInfo);
+
+            // 把新客户端添加进监听可读事件列表中
+            FD_SET(connfd, &allset);
+
+            // 给 select 用
+            if (connfd > maxfd)
+                maxfd = connfd;
+
+            // 给查找客户端 connfd 通信用
+            if (i > maxi)
+                maxi = i;
+
+            if (--nready <= 0) 
+                continue;
+        }
+
+        printf("connfd\n");
+
+        for (int j = 0; j <= maxi; j++) {
+
+            if (clifds[j] < 0) 
+                continue;
+            
+            if (FD_ISSET(clifds[j], &rset)) {
+                if ((nread = read(clifds[j], buf, sizeof(buf))) == 0) {
+                    close(clifds[j]);
+                    FD_CLR(clifds[j], &allset);
+                    clifds[j] = -1;
+                } else if (nready < 0) {
+                    /* 为什么会发生这种情况呢？ */
+                } else {
+                    if ((nwrite = write(clifds[j], buf, nread)) != nread) {
+                        perror("write error!");
+                        exit(1);
+                    }
+                }
+                if (--nready <= 0)
+                    break;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ UNP/06 IO复用 selet 和 poll 函数/progs/Practise_08.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <errno.h>
+#include "AddrTool.h"
+
+#define OPEN_MAX 1024
+
+int main(int argc, char const *argv[])
+{
+    int listenfd = -1;
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket error!");
+        exit(1);
+    }
+
+    struct sockaddr_in svraddr, cliaddr;
+    bzero(&svraddr, sizeof(svraddr));
+    svraddr.sin_family = AF_INET;
+    svraddr.sin_port = htons(atoi(argv[1]));
+    svraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(listenfd, (struct sockaddr *)&svraddr, sizeof(svraddr)) < 0) {
+        perror("bind error!");
+        exit(1);
+    }
+
+    if (listen(listenfd, 100) < 0) {
+        perror("listenfd error!");
+        exit(1);
+    }
+
+    int open_max = 0;
+    if ((open_max = sysconf(_SC_OPEN_MAX)) < 0) {
+        perror("sysconf error!");
+        exit(1);
+    }
+
+    struct pollfd *clients = NULL;
+    if ((clients = malloc(sizeof(struct pollfd) * open_max)) == NULL) {
+        printf("malloc error!\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < open_max; i++) 
+        clients[i].fd = -1;
+
+    clients[0].fd = listenfd;
+    clients[0].events = POLLRDNORM;
+
+    int connfd = -1;
+    int nready = 0;
+    int maxi = 0;
+    socklen_t clilen = sizeof(cliaddr);
+    char buf[1024];
+    for (;;) {
+        if ((nready = poll(clients, maxi + 1, -1)) < 0) {
+            perror("poll error!");
+            exit(1);
+        }
+
+        /* 监听套接字 */
+        if (clients[0].revents & POLLRDNORM) {
+            if ((connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen)) < 0) {
+                perror("accept error!");
+                exit(1);
+            }
+
+            char *peerinfo = getPeerInfo(connfd);
+            char *sockinfo = getSockInfo(connfd);
+            printf("%s <---> %s established TCP connection\n", sockinfo, peerinfo);
+            free(peerinfo);
+            free(sockinfo);
+
+            int i = 1;
+            for (; i < open_max; i++) {
+                if (clients[i].fd < 0) {
+                    clients[i].fd = connfd;
+                    clients[i].events = POLLRDNORM;
+                    break;
+                }
+            }
+            if (i == open_max) {
+                printf("too many fds\n");
+                exit(1);
+            }
+
+            if (i > maxi)
+                maxi = i;
+
+            if (--nready <= 0)
+                continue;
+        }
+
+        /* 客户端套接字 */
+        for (int i = 1; i <= maxi; i++) {
+            if (clients[i].revents & POLLRDNORM) {
+                ssize_t nread = -1, nwrite = -1;
+                if ((nread = read(clients[i].fd, buf, sizeof(buf))) < 0) {
+                    if (errno == ECONNRESET) {
+
+                        char *peerinfo = getPeerInfo(connfd);
+                        printf("TCP connection reset by peer %s\n", peerinfo);
+                        free(peerinfo);
+
+                        close(clients[i].fd);
+                        clients[i].fd = -1;
+
+                    } else {
+                        perror("read error!");
+                        exit(1);
+                    }
+                } else if (nread == 0) {
+                    char *peerinfo = getPeerInfo(connfd);
+                    printf("client cut connection [fd=%d][peerinf=%s]\n", clients[i].fd, peerinfo);
+                    free(peerinfo);
+                    close(clients[i].fd);
+                    clients[i].fd = -1;
+                } else {
+                    if ((nwrite = write(clients[i].fd, buf, nread)) != nread) {
+                        perror("nwrite error!");
+                        exit(1);
+                    }
+                }
+
+                if (--nready <= 0) 
+                    break;
+            }
+        }
+    }
+
+    return 0;
+}
