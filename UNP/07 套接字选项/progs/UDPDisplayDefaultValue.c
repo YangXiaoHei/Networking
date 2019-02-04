c275526953594804f2c8c0186a31b7f437f6c43a
yangxiaohei <yangxiaohei321123@163.com>
2019-02-04 22:21:28 +0800

    习题

---
+++ UNP/07 套接字选项/progs/UDPDisplayDefaultValue.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <string.h>
+
+void display(const char *name, int option) 
+{
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    long val = 0;
+    socklen_t len = sizeof(val);
+    if (getsockopt(fd, SOL_SOCKET, option, &val, &len) < 0) {
+        perror("getsockopt error!");
+        exit(1);
+    }
+    printf("%s : default = %ld\n", name, val);
+    close(fd);
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /*
+     * SO_KEEPALIVE
+     * SO_SNDBUF
+     * SO_RCVBUF
+     * SO_LINGER
+     * SO_RCVLOWAT
+     * SO_SNDLOWAT
+     * SO_REUSEADDR
+     * SO_REUSEPORT
+    * SO_RCVTIMEO
+    * SO_SNDTIMEO   
+     */
+
+    display("SO_KEEPALIVE", SO_KEEPALIVE);
+    display("SO_SNDBUF", SO_SNDBUF);
+    display("SO_RCVBUF", SO_RCVBUF);
+    display("SO_LINGER", SO_LINGER);
+    display("SO_RCVLOWAT", SO_RCVLOWAT);
+    display("SO_SNDLOWAT", SO_SNDLOWAT);
+    display("SO_REUSEADDR", SO_REUSEADDR);
+    display("SO_REUSEPORT", SO_REUSEPORT);
+    display("SO_SNDTIMEO", SO_SNDTIMEO);
+    display("SO_RCVTIMEO", SO_RCVTIMEO);
+
+    return 0;
+}
