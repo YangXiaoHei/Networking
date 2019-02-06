9095c418bc6ab93501c51efb9ba7052e4bbae9e4
yangxiaohei <yangxiaohei321123@163.com>
2019-02-06 20:24:46 +0800

    一个失败的例子，进程间的同步不太好弄

---
+++ UNP/11 名字与地址转换/progs/Practise_02_client.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include "AddrTool.h"
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 3) {
+        printf("usage : %s <ip/hostname> <port/service>\n", argv[0]);
+        exit(1);
+    }
+
+    const char *hostname = argv[1];
+    const char *service = argv[2];
+
+    struct in_addr **pptr;
+    struct in_addr *inetaddrp[2];
+
+    struct in_addr inetaddr;
+    struct hostent *ent = NULL;
+    if ((ent = gethostbyname(hostname)) == NULL) {
+        if (inet_aton(hostname, &inetaddr) == 0) {
+            perror("inet_aton error!");
+            exit(1);
+        } else {
+            inetaddrp[0] = &inetaddr;
+            inetaddrp[1] = NULL;
+            pptr = inetaddrp;
+        }   
+    } else {
+        pptr = (struct in_addr **)ent->h_addr_list;
+    }
+
+    struct servent *svrent = NULL;
+    if ((svrent = getservbyname(service, "tcp")) == NULL) {
+        perror("getservbyname error!");
+        exit(1);
+    }
+
+    int fd = -1;
+    struct sockaddr_in svraddr;
+    for (; *pptr != NULL; pptr++) {
+        if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            perror("socket fd create fail!");
+            continue;
+        }
+
+        bzero(&svraddr, sizeof(svraddr));
+        svraddr.sin_family = AF_INET;
+        memcpy(&svraddr.sin_addr, *pptr, sizeof(struct in_addr));
+        svraddr.sin_port = svrent->s_port;
+
+        if (connect(fd, (struct sockaddr *)&svraddr, sizeof(svraddr)) < 0) {
+            perror("connect error!");
+            continue;
+        }
+
+        char *addrInfo = getAddrInfo(&svraddr);
+        printf("connect to %s succ!\n", addrInfo);
+        free(addrInfo);
+        break;
+    }
+
+    char buf[1024];
+    ssize_t nread = 0;
+    while ((nread = read(fd, buf, sizeof(buf))) > 0) {
+        buf[nread] = 0;
+        printf("%s\n", buf);
+    }
+
+    return 0;
+}
--- UNP/11 名字与地址转换/progs/Practise_02_server.c
+++ UNP/11 名字与地址转换/progs/Practise_02_server.c
@@ -1,12 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <signal.h>
+#include <sys/select.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/wait.h>
+
+typedef void(*sig_handler)(int);
+
+sig_handler fuck(int signo, sig_handler newhandler) 
+{
+    struct sigaction newact, oldact;
+    bzero(&newact, sizeof(newact));
+    newact.sa_flags = 0;
+    sigemptyset(&newact.sa_mask);
+    newact.sa_handler = newhandler;
+
+    if (signo == SIGALRM) {
+#ifdef SA_INTERRUPT
+        newact.sa_flags |= SA_INTERRUPT;
+#endif
+    } else {
+#ifdef SA_RESTART
+        newact.sa_flags |= SA_RESTART;
+#endif
+    }
+
+    if (sigaction(signo, &newact, &oldact) < 0)
+        return SIG_ERR;
+    return oldact.sa_handler;
+}
+
+void child_collect(int signo) 
+{
+    pid_t pid = 0;
+    int status;
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        printf("collcet child %ld\n", (long)pid);
+    }
+    if (pid < 0 && errno != ECHILD) {
+        perror("waitpid error!");
+    }
+}
+
+
 int main(int argc, char const *argv[])
 {
-    if (argc != 3) {
-        printf("usage : %s <ip/hostname> <port/service>\n", argv[0]);
+    if (argc != 2 && argc != 3) {
+        printf("usage : %s [ip/hostname] <port/service>\n", argv[0]);
         exit(1);
     }
 
-    const char *hostname = argv[1];
-    const char *service = argv[2];
+    setbuf(stdout, NULL);
+
+    const char *hostname = NULL;
+    const char *service = NULL;
+
+    if (argc == 2) {
+        hostname = NULL;
+        service = argv[1]; 
+    } else {
+        hostname = argv[1];
+        service = argv[2];
+    }
 
     struct addrinfo hints, *res = NULL, *ressave = NULL;
     bzero(&hints, sizeof(hints));
@@ -22,7 +83,6 @@ int main(int argc, char const *argv[])
     ressave = res;
 
     int listenfd = -1, udpfd = -1;
-
     do {    
         if (res->ai_socktype == SOCK_DGRAM) {
 
@@ -52,7 +112,7 @@ int main(int argc, char const *argv[])
 
             if (listenfd >= 0) {
                 printf("same port use for 2 tcp service\n!");
-                exitt(1);
+                exit(1);
             }
 
             if ((listenfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0) {
@@ -72,6 +132,11 @@ int main(int argc, char const *argv[])
                 close(listenfd);
             }
 
+            if (listen(listenfd, 1000) < 0) {
+                perror("tcp fd listen error!");
+                close(listenfd);
+            }
+
         } else {
             printf("not supported socket type!\n");
             continue;
@@ -82,6 +147,9 @@ int main(int argc, char const *argv[])
         printf("%s %s server start fail!\n", hostname, service);
         exit(1);
     }
+    printf("listenfd = %d udpfd = %d\n", listenfd, udpfd);
+
+    fuck(SIGCHLD, child_collect);
 
     int fds[1024];
     for (int i = 0; i < 1024; i++)
@@ -100,15 +168,21 @@ int main(int argc, char const *argv[])
     int connfd = -1;
     char *msg = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     size_t msglen = strlen(msg);
-    srand((unsigned int)time(NULL)); 
     int nready = 0;
-    int maxi = 1;
+    int maxi = 2;
     int maxfd = listenfd > udpfd ? listenfd : udpfd;
+
+    pid_t pids[1024];
+    for (int i = 0; i < 1024; i++)
+        pids[i] = -1;
+
     for (;;) {
+        rset = allset;
         if ((nready = select(maxfd + 1, &rset, NULL, NULL, NULL)) < 0) {
             perror("select error!");
             continue;
         }
+        printf("%d fds readable\n", nready);
 
         /* udpfd */
         if (FD_ISSET(fds[0], &rset)) {
@@ -118,15 +192,17 @@ int main(int argc, char const *argv[])
                 if (--nready <= 0)
                     continue;
             } else if (pid == 0) {
+                printf("new process created for udp: pid=%ld\n", (long)pid);
+                srand((unsigned int)time(NULL)); 
                 char c = 0;
                 struct sockaddr_in cliaddr;
                 socklen_t clilen = sizeof(cliaddr);
-                if (recvfrom(fds[0], &c, 1, &cliaddr, &clilen) < 0) {
+                if (recvfrom(fds[0], &c, 1, 0, (struct sockaddr *)&cliaddr, &clilen) < 0) {
                     perror("recvfrom error!");
                     exit(1);
                 }
 
-                if (connect(fds[0], &cliaddr, clilen) < 0) {
+                if (connect(fds[0], (struct sockaddr *)&cliaddr, clilen) < 0) {
                     perror("connect to client error!");
                     exit(1);
                 }
@@ -134,9 +210,10 @@ int main(int argc, char const *argv[])
                 for (;;) {
                     int ridx = rand() % msglen;
                     ssize_t nwrite = 0;
-                    if ((nwrite = write(fds[0], msg[ridx], 1)) <= 0) {
+                    if ((nwrite = write(fds[0], &msg[ridx], 1)) <= 0) {
                         perror("write error!");
-                        exit(1);
+                        printf("child process exit with code 0 pid=%ld\n", (long)getpid());
+                        exit(0);
                     }
                 }
             }
@@ -144,12 +221,92 @@ int main(int argc, char const *argv[])
 
         /* listenfd */
         if (FD_ISSET(fds[1], &rset)) {
+            struct sockaddr_in cliaddr;
+            socklen_t clilen = sizeof(clilen);
+            if ((connfd = accept(fds[1], (struct sockaddr *)&cliaddr, &clilen)) < 0) {
+                perror("accept error!");
+                --nready;
+                continue;
+            }
 
+            int i = 2;
+            for (; i < 1024; i++) {
+                if (fds[i] < 0) {
+                    fds[i] = connfd;
+                    FD_SET(connfd, &allset);
+                    break;
+                }
+            }
+            if (i == 1024) {
+                printf("too many connections\n");
+                close(connfd);
+                if (--nready <= 0) 
+                    continue;
+            }
+
+            if (connfd > maxfd)
+                maxfd = connfd;
+
+            if (i > maxi)
+                maxi = i;
+
+            pid_t pid = 0;
+            if ((pid = fork()) < 0) {
+                perror("fork error!");
+            } else if (pid == 0) {
+                printf("new process created for udp: pid=%ld\n", (long)pid);
+                srand((unsigned int)time(NULL)); 
+                for (;;) {
+                    int ridx = rand() % msglen;
+                    ssize_t nwrite = 0;
+                    if ((nwrite = write(connfd, &msg[ridx], 1)) != 1) {
+                        if (errno == EPIPE) {
+                            close(connfd);
+                            printf("client cut connection\n");
+                            printf("child process exit with code 0 pid=%ld\n", (long)getpid());
+                            exit(0);
+                        }
+                    }
+                }
+            } 
+
+            pids[connfd] = pid;
+
+            if (--nready <= 0)
+                continue;
         }
 
+        for (int i = 2; i <= maxi; i++) {
+            if (fds[i] < 0) 
+                continue;
+
+            if (FD_ISSET(fds[i], &rset)) {
+                char c = 0;
+                ssize_t nread = 0;
+                if ((nread = read(fds[i], &c, 1)) < 0) {
+                    if (errno == ECONNRESET) {
+                        close(connfd);
+                        printf("client cut connection\n");
+                        if (pids[connfd] > 0) {
+                            printf("kill child process for tcp: pid=%ld\n", (long)pids[connfd]);
+                            kill(pids[connfd], SIGKILL);
+                            pids[connfd] = -1;
+                        }
+                    }
+                } else if (nread == 0) {
+                    close(connfd);
+                    printf("client cut connection\n");
+                    if (pids[connfd] > 0) {
+                        printf("kill child process for tcp: pid=%ld\n", (long)pids[connfd]);
+                        kill(pids[connfd], SIGKILL);
+                        pids[connfd] = -1;
+                    }
+                }
+
+                if (--nready <= 0)
+                    continue;
+            }
+        }
     }
-    
-
-
     return 0;
 }
