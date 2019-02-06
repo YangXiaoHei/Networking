39ebacae76de5ea46c9189a5458667107f520951
yangxiaohei <yangxiaohei321123@163.com>
2019-02-06 17:11:08 +0800

    1 ~ 2 习题

---
+++ UNP/11 名字与地址转换/progs/Practise_01.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include "AddrTool.h"
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2) {
+        printf("usage : %s <hostname>\n", argv[0]);
+        exit(1);
+    }
+
+    const char *hostname = argv[1];
+    struct hostent *ent = NULL;
+
+    if ((ent = gethostbyname(hostname)) == NULL) {
+        printf("gethostname error! %s : %s", hostname, hstrerror(h_errno));
+        exit(1);
+    }
+
+    ent->h_length = 999;
+
+    struct hostent *eachent = NULL;
+    for (char **pptr = ent->h_addr_list; *pptr != NULL; pptr++) {
+        if ((eachent = gethostbyaddr((struct in_addr *)*pptr, 4, ent->h_addrtype)) == NULL) {
+            char *ip = inet_ntoa(*(struct in_addr *)*pptr);
+            printf("gethostbyaddr error! %s : %s\n", ip, hstrerror(h_errno));
+            continue;
+        }
+        printf("%d\n", ent->h_length);
+        char *ip = inet_ntoa(*(struct in_addr *)*pptr);
+        printf("%s -> h_name = %s\n", ip, eachent->h_name);
+    }
+    return 0;
+}
