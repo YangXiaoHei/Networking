69996181bc150fd4cfc13be99725960b6f795161
yangxiaohei <yangxiaohei321123@163.com>
2019-02-07 19:08:52 +0800

    由 xinetd 启动的 udp 和 tcp 版本的时间获取服务器

---
+++ UNP/13 守护进程和 inetd 超级服务器/progs/TCPDaytimeServerExecByXinetd.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <syslog.h>
+#include <time.h>
+#include <errno.h>
+#include "AddrTool.h"
+
+int main(int argc, char const *argv[])
+{
+    struct sockaddr_storage cliaddr;
+    socklen_t len = sizeof(cliaddr);
+    if (getpeername(0, (struct sockaddr *)&cliaddr, &len) < 0) {
+        syslog(LOG_INFO, "getpeername error! %s\n", strerror(errno));
+        exit(1);
+    }
+
+    openlog(argv[0], LOG_PID, LOG_INFO);
+
+    char *addrInfo = getAddrInfo((struct sockaddr_in *)&cliaddr);
+    syslog(LOG_INFO, "tcp connection from %s", addrInfo);
+    free(addrInfo);
+
+    time_t ticks = time(NULL);
+    char buf[1024];
+    snprintf(buf, sizeof(buf), "%.24s", ctime(&ticks));
+    if (write(0, buf, strlen(buf)) < 0) {
+        syslog(LOG_INFO, "write error! : %s\n", strerror(errno));
+    }
+    close(0);
+    exit(0);
+}
---
+++ UNP/13 守护进程和 inetd 超级服务器/progs/UDPDaytimeServerExecByXinetd.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <syslog.h>
+#include <time.h>
+#include <errno.h>
+#include "AddrTool.h"
+
+int main(int argc, char const *argv[])
+{
+    struct sockaddr_storage cliaddr;
+    socklen_t len = sizeof(cliaddr);
+    
+    openlog(argv[0], LOG_PID, LOG_INFO);
+
+    char c = 0;
+    if (recvfrom(0, &c, 1, 0, (struct sockaddr *)&cliaddr, &len) < 0) {
+        syslog(LOG_INFO, "recvfrom error!");
+        exit(1);
+    }
+
+    char *addrInfo = getAddrInfo((struct sockaddr_in *)&cliaddr);
+    syslog(LOG_INFO, "udp req from %s\n", addrInfo);
+    free(addrInfo);
+
+    time_t ticks = time(NULL);
+    char buf[1024];
+    snprintf(buf, sizeof(buf), "%.24s", ctime(&ticks));
+    if (sendto(0, buf, strlen(buf), 0, (struct sockaddr *)&cliaddr, len) < 0) {
+        syslog(LOG_INFO, "sento error! : %s\n", strerror(errno));
+        exit(1);
+    }
+    close(0);
+    exit(0);
+}
