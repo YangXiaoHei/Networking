afc01a03dedfa89cd5d127a6a8c2d5f0102b994c
yangxiaohei <yangxiaohei321123@163.com>
2019-02-08 16:09:56 +0800

    测试 readv 的两个客户端，一个一次性发送 26 个字节的分组；另一个禁用 Nagle 算法并发送 26 个单字节分组

---
+++ UNP/14 高级 IO 函数/progs/TCPReadvTestClient_WriteAllOneTime.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 3) {
+        printf("usage : %s <ip> <port>\n", argv[0]);
+        exit(1);
+    }
+
+    int fd = -1;
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket fd create fail!");
+        exit(1);
+    }
+
+    struct sockaddr_in svraddr;
+    bzero(&svraddr, sizeof(svraddr));
+    svraddr.sin_family = AF_INET;
+    svraddr.sin_port = htons(atoi(argv[2]));
+    svraddr.sin_addr.s_addr = inet_addr(argv[1]);
+
+    int on = 1;
+    if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on)) < 0) {
+        perror("setsockopt error!");
+        exit(1);
+    }
+
+    if (connect(fd, (struct sockaddr *)&svraddr, sizeof(svraddr)) < 0) {
+        perror("connect error!");
+        exit(1);
+    }
+
+    char *msg = "abcdefghijklmnopqrstuvwxyz";
+
+    if (write(fd, msg, strlen(msg)) != strlen(msg)) {
+        perror("write error!");
+        exit(1);
+    }
+    
+    
+    return 0;
+}
