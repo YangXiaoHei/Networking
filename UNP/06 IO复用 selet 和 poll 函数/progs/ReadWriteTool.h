ecc12bb1f7473b6d83f5451001efb388467a7689
yangxiaohei <yangxiaohei321123@163.com>
2019-02-21 14:45:36 +0800

    统一工具路径

--- UNP/06 IO复用 selet 和 poll 函数/progs/ReadWriteTool.h
+++
@@ -1,9 +0,0 @@
-#ifndef _READ_WRITE_TOOL_H
-#define _READ_WRITE_TOOL_H
-
-#include <stdio.h>
-
-ssize_t readline(int fd, char *buf, size_t maxlen);
-ssize_t writen(int fd, char *ptr, size_t len);
-
-#endif
--- UNP/16 非阻塞式 IO/progs/TimeTool.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TIME_TOOL_H
-#define _TIME_TOOL_H
-
-const char *curtimestr();
-unsigned long curtimeus();
-char *tcpdump_timestamp(void);
-ssize_t logx(const char *fmt, ...);
-
-#endif
