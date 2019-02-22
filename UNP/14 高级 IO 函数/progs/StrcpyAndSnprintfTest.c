599c6aa4199decbf38bf877d5aa22aa91524552f
yangxiaohei <yangxiaohei321123@163.com>
2019-02-22 23:04:24 +0800

    证明 strncpy 不会自动添加 \0

--- UNP/14 高级 IO 函数/progs/StrcpyAndSnprintfTest.c
+++ UNP/14 高级 IO 函数/progs/StrcpyAndSnprintfTest.c
@@ -5,10 +5,16 @@ int main(int argc, char const *argv[])
 {
     char *msg = "hansonyang";
     char buf[64];
-    memset(buf, 'a', sizeof(buf));
+    
 
     /* strcpy 会自动在 buf 后填 \0 */
+    memset(buf, 'a', sizeof(buf));
     strcpy(buf, msg);
+    printf("%s\n", buf);
+
+    /* strncpy 不会自动添加 \0 */
+    memset(buf, 'a', sizeof(buf));
+    strncpy(buf, msg, strlen(msg));
     printf("%s\n", buf);
 
     /* snprintf 也会自动在 buf 后填 \0 */
