c9cfa6e94d7c3c76882b7f4331b77bf45eb27a7f
yangxiaohei <yangxiaohei321123@163.com>
2019-02-24 23:22:14 +0800

    观察流量控制 - 服务器的修改，命令行参数设置接收缓冲区大小

--- UNP/24 带外数据/progs/FlowControlTestServer.c
+++ UNP/24 带外数据/progs/FlowControlTestServer.c
@@ -8,8 +8,8 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
-        printf("usage : %s <port>\n", argv[0]);
+    if (argc != 3) {
+        printf("usage : %s <port> <recvbuff kb>\n", argv[0]);
         exit(1);
     }
     int fd = -1;
@@ -23,11 +23,11 @@ int main(int argc, char *argv[])
     svraddr.sin_port = htons(atoi(argv[1]));
     svraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     
-    ssize_t rcvbuf = 1 << 10;
+    ssize_t rcvbuf = atoi(argv[2]) << 10;
     if (setsockopt(fd, SOL_SOCKET, SO_RCVBUF, &rcvbuf, sizeof(rcvbuf)) < 0) {
         perror("setsockopt error!");
         exit(1);
-    }   
+    }
     
     if (bind(fd, (struct sockaddr *)&svraddr, sizeof(svraddr)) < 0) {
         perror("bind error!");
