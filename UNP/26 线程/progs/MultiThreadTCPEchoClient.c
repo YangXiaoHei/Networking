a23c224d00b656038419e802d09e274f5559de94
yangxiaohei <yangxiaohei321123@163.com>
2019-02-28 20:25:21 +0800

    多线程下载客户端 bug 修复

--- UNP/26 线程/progs/MultiThreadTCPEchoClient.c
+++ UNP/26 线程/progs/MultiThreadTCPEchoClient.c
@@ -7,6 +7,9 @@
 #include <errno.h>
 #include <pthread.h>
 
+static pthread_t tid;
+static ssize_t total_read;
+
 int tcp_connect(const char *ip, unsigned short port)
 {
     int fd = -1;
@@ -29,16 +32,33 @@ void *recvTextFromServer(void *arg)
     int fd = (int)arg;
     char buf[1024];
     ssize_t nwrite, nread;
+    /* setbuf(stdout, NULL); */
     for (;;) {
         if ((nread = read(fd, buf, sizeof(buf))) < 0) {
             perror("server terminated abnormal");
-            exit(1);
+            break;
         } else if (nread == 0) {
-            printf("server cut connection\n");
+            fprintf(stderr, "server cut connection, total_read=%zd\n", total_read);
+            break;
+        }
+        total_read += nread;
+        /* fprintf(stderr, "recv %zd bytes from server, total_read=%zd\n", nread, total_read); */
+        /* buf[nread] = 0; */
+        /* if ((nwrite = write(STDOUT_FILENO, buf, nread)) != nread) { */
+            perror("write error!");
             exit(1);
         }
-        buf[nread] = 0;
-        printf("%s", buf);
+        /*
+         * printf("%s", buf);
+         */
+        /*
+         * if (fputs(buf, stdout) == EOF) {
+         *     if (ferror(stdout)) {
+         *         printf("stdout error!");
+         *         exit(1);
+         *     }
+         * }
+         */
     }
     return (void *)NULL;
 }
@@ -49,7 +69,6 @@ void sendTextToServer(int fd)
     ssize_t nread, nwrite;
 
     int error = 0;
-    pthread_t tid;
     if ((error = pthread_create(&tid, NULL, recvTextFromServer, (void *)fd)) != 0) {
         perror("pthread_Create fail!");
         exit(1);
@@ -63,7 +82,6 @@ void sendTextToServer(int fd)
     }
     if (nread == 0) {
         shutdown(fd, SHUT_WR);
-        exit(0);
     } else {
         perror("read error!");
         exit(1);
@@ -73,13 +91,21 @@ void sendTextToServer(int fd)
 int main(int argc, char *argv[])
 {
     if (argc != 3) {
-        printf("usage : %s <ip> <port>\n", argv[0]);
+        fprintf(stderr, "usage : %s <ip> <port>\n", argv[0]);
         exit(1);
     }
+    /* setbuf(stdout, NULL); */
     int fd = -1;
     if ((fd = tcp_connect(argv[1],atoi(argv[2]))) < 0) {
         perror("tcp connect error!");
         exit(1);
     }
     sendTextToServer(fd);
+    
+    int n = 0;
+    if ((n = pthread_join(tid, NULL)) != 0) {
+        fprintf(stderr, "pthread_join fail! : %s\n", strerror(n));
+        exit(0);
+    }   
+ 
 }
