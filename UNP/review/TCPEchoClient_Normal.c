c3c4ef12446258851f70131535334ffffd88c984
yangxiaohei <yangxiaohei321123@163.com>
2019-03-12 21:37:15 +0800

    tcp 回显客户端，停等版本 stop and wait

--- UNP/review/TCPEchoClient_Normal.c
+++ UNP/review/TCPEchoClient_Normal.c
@@ -12,13 +12,13 @@
 int main(int argc, char *argv[])
 {
     if (argc != 3) {
-        logx("usage : %s <ip/hostname> <port/service>", argv[0]);
+        mlogx("usage : %s <ip/hostname> <port/service>", argv[0]);
         exit(1);
     }
 
     int fd = -1;
     if ((fd = tcp_connect_cb(argv[1], argv[2], NULL)) < 0) {
-        logx("tcp_connect_cb error!", strerror(errno));
+        mlogx("tcp_connect_cb error!", strerror(errno));
         exit(1);
     }
 
@@ -30,36 +30,36 @@ int main(int argc, char *argv[])
         /* 从标准输入读取，并发送到服务器 */
         if (fgets(buf, sizeof(buf), stdin) == NULL) {
             if (ferror(stdin)) 
-                logx("fgets error!");
+                mlogx("fgets error!");
             else
-                logx("client send data finish!");
+                mlogx("client send data finish!");
             break;
         }
 
         /* 写到服务器 */
         ntowrite = strlen(buf);
         if (writen(fd, buf, ntowrite) != ntowrite) {
-            logx("writen error! %s", strerror(errno));
+            mlogx("writen error! %s", strerror(errno));
             break;
         }
-        logx("send %zd bytes to server", ntowrite);
+        mlogx("send %zd bytes to server", ntowrite);
 
         /* 从服务器读取回显响应 */
         if ((nread = readn(fd, buf, ntowrite)) <= 0) {
             if (nread < 0)
-                logx("readn error! %s", strerror(errno));
+                mlogx("readn error! %s", strerror(errno));
             else
-                logx("server close connection");
+                mlogx("server close connection");
             break;
         }
-        logx("recv %zd bytes from server", ntowrite);
+        mlogx("recv %zd bytes from server", ntowrite);
 
         /* 将回显响应写到标准输出 */
         if (writen(STDOUT_FILENO, buf, ntowrite) != ntowrite) {
-            logx("writen error! %s", strerror(errno));
+            mlogx("writen error! %s", strerror(errno));
             break;
         }
     }
-    logx("client close connection");
+    mlogx("client close connection");
     close(fd);
 }
