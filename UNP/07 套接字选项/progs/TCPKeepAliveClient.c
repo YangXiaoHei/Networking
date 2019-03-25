46f0bb74a7c9e128383debbf66421dcb2dd0171a
yangxiaohei <yangxiaohei321123@163.com>
2019-03-25 21:01:44 +0800

    TCP_KEEPIDLE 这几个选项没啥效果啊。。。

--- UNP/07 套接字选项/progs/TCPKeepAliveClient.c
+++ UNP/07 套接字选项/progs/TCPKeepAliveClient.c
@@ -3,11 +3,40 @@
 #include <unistd.h>
 #include <string.h>
 #include "../../tool/CommonTool.h"
+#include <netinet/tcp.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int cb(int fd)
+{
+    int on = 1;
+    int interval = 60;
+    int idle = 5;
+    int count = 3;
+
+    if (setsockopt(fd, IPPROTO_TCP, SO_KEEPALIVE, &on, sizeof(on)) < 0) {
+        perror("setsockopt error!");
+        return -1;
+    }
+    if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPIDLE, &idle, sizeof(idle)) < 0) {
+        perror("setsockopt error!");
+        return -2;
+    }
+    if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPINTVL, &interval, sizeof(interval)) < 0) {
+        perror("setsockopt error!");
+        return -3;
+    }
+    if (setsockopt(fd, IPPROTO_TCP, TCP_KEEPCNT, &count, sizeof(count)) < 0) {
+        perror("setsockopt error!");
+        return -4;
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
     int fd = -1;
-    if ((fd = tcp_connect(argv[1], argv[2])) < 0) {
+    if ((fd = tcp_connect_cb(argv[1], argv[2], cb)) < 0) {
         perror("tcp_connect error!");
         exit(1);
     }
